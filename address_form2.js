d90ea38591740c2c22098c333c2155b3a1c4712c
Clayton <c_w_serious-github@yahoo.com.au>
2019-04-25 17:40:23 +1000

    street name autocomplete working

--- address_form2.js
+++ address_form2.js
@@ -1,9 +1,9 @@
 var placeSearch, autocomplete;
 
 var componentForm = {
-  street_number: 'long_name'//,
-  /*
-  route: 'long_name',
+  street_number: 'long_name',
+  route: 'long_name'
+  /*,
   locality: 'long_name',
   administrative_area_level_1: 'short_name',
   country: 'long_name',
@@ -30,16 +30,9 @@ function fillInAddress() {
   // Get the place details from the autocomplete object.
   var place = autocomplete.getPlace();
   console.log(place);
-  /*
-  for (var component in componentForm) {
-    document.getElementById(component).value = '';
-    document.getElementById(component).disabled = false;
-  }
-  */
-
+  
   // Get each component of the address from the place details,
   // and then fill-in the corresponding field on the form.
-  
   for (var i = 0; i < place.address_components.length; i++) {
     var addressType = place.address_components[i].types[0];
     if (componentForm[addressType]) {
@@ -47,5 +40,4 @@ function fillInAddress() {
       document.getElementById(addressType).value = val;
     }
   }
-  
 }
