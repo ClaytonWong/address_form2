ea8cdd7ebbec85b64655eb2e135bebe8f0bc232d
Clayton <c_w_serious-github@yahoo.com.au>
2019-04-25 22:25:24 +1000

    postcode input id changed to postal_code

--- address_form2.js
+++ address_form2.js
@@ -2,13 +2,14 @@ var placeSearch, autocomplete;
 
 var componentForm = {
   street_number: 'long_name',
-  route: 'long_name'
-  /*,
+  route: 'long_name', // Street name
+  /*
   locality: 'long_name',
-  administrative_area_level_1: 'short_name',
+  administrative_area_level_1: 'long_name',
   country: 'long_name',
-  postal_code: 'short_name'
   */
+  postal_code: 'long_name'
+  
 };
 
 function initAutocomplete() {
